681d4ffb2b282fdb230c800b687e3e6ef69d6bea
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 12:01:15 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -6,11 +6,11 @@ import PackageDescription
 let package = Package(
     name: "swift-w3c-xml",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(name: "W3C XML", targets: ["W3C XML"])
